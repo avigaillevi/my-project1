1b1600029fe73d7650f324ae3fa889cccad298a8
Avigail Levi <avigaillevi45@gmail.com>
2026-06-02 12:43:31 +0300

    add screenshots

---
+++ src/components/ErrorBoundaries.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
